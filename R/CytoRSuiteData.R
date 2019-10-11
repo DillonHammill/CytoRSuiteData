b632f48ea03f954b928c6c24f32db30d774f9b11
Dillon Hammill <dillonh21@live.com.au>
2019-10-11 19:48:54 +1100

    Revert "Updates for CytoExploreR"

--- R/CytoRSuiteData.R
+++ R/CytoRSuiteData.R
@@ -18,7 +18,7 @@ NULL
 
 
 
-#' CytoRSuite gatingTemplate used to gate the Activation data set.
+#' CytoRSuite gatingTemplate used to gate the Activation dataset.
 #' @name Activation_gatingTemplate
 #' @docType data
 #' @title CytoRSuite Activation gatingTemplate.
@@ -27,8 +27,8 @@ NULL
 #'   \item{\strong{Live Cells}} \item{\strong{T Cells}} \item{\strong{Dendritic
 #'   Cells}} \item{\strong{CD4 T Cells}} \item{\strong{CD8 T Cells}}
 #'   \item{\strong{CD69+ CD4 T Cells}} \item{\strong{CD69+ CD8 T Cells}} }
-#' @source The Activation_gatingTemplate was generated using CytoRSuite to
-#'   analyse Activation data set.
+#' @source The Activation_gatingTemplate was generated using CytoRSuite and the
+#'   Activation dataset.
 #' @seealso \link{CytoRSuiteData}
 #' @seealso \link{Compensation}
 #' @seealso \link{Activation}
@@ -41,20 +41,18 @@ NULL
 
 
 
-#' @title CytoRSuite Compensation Data Set.
+#' @title CytoRSuite Compensation Dataset.
 #' @description A collection of single stain compensation controls and an
-#'   unstained control used to compensate the Activation data set.
+#'   unstained control used in CytoRSuite for testing and demonstration
+#'   purposes.
 #' @name Compensation
 #' @docType data
-#' @format A \code{flowSet} containing the 7 compensation controls.
+#' @format A \code{flowSet} containing the 6 compensation controls.
 #' @details Compensation controls are as follows: \itemize{
-#'   \item{\strong{Compensation-7AAD.fcs}}
-#'   \item{\strong{Compensation-AF700.fcs}} \item{\strong{Compensation-APC.fcs}}
-#'   \item{\strong{Compensation-APC-Cy7.fcs}}
-#'   \item{\strong{Compensation-FITC.fcs}} \item{\strong{Compensation-PE.fcs}}
-#'   \item{\strong{Compensation-Unstained.fcs}}}
-#' @source Compensation controls used for an in vitro OT-I/II T cell activation
-#'   assay.
+#'   \item{\strong{7AAD.fcs}} \item{\strong{AF-430.fcs}}
+#'   \item{\strong{APC-Cy7.fcs}} \item{\strong{NIL.fcs}{  (Unstained)}}
+#'   \item{\strong{PE-Cy7.fcs}} \item{\strong{PE.fcs}} }
+#' @source Some compensation controls from an experiment.
 #' @seealso \link{CytoRSuiteData}
 #' @seealso \link{Activation}
 #' @seealso \link{Activation_gatingTemplate}
@@ -65,32 +63,27 @@ NULL
 
 
 
-#' @title CytoRSuite Activation Data Set
+#' @title CytoRSuite Activation Dataset
 #' @description Data from an in vitro OT-I/II T cell activation assay.
 #' @name Activation
 #' @docType data
-#' @format a \code{flowSet} containing 33 samples with 4 OVA concentrations
-#'   (0,0.005,0.05 and 0.5) and 4 distinct treatments
-#'   ("Stim-A","Stim-B","Stim-C" and "Stim-D") in duplicate. The last sample is
-#'   a universal unstained control which can be used to aid in setting
-#'   appropriate gates.
+#' @format a \code{flowSet} containing 4 samples in order of increasing OVA
+#'   concentration.
 #' @details \describe{ An experiment performed to assess the in vitro activation
 #'   of OT-I and OT-II transgenic T cells co-cultured with OVA-presenting
-#'   dendritic cells (DCs). This data set is comprised of 32 samples saved as a
-#'   \code{flowSet} object. Samples include 4 OVA concentrations and 4 distinct
-#'   treatment groups in duplicate.
+#'   dendritic cells (DCs). This dataset is comprised of 4 samples saved as a
+#'   \code{flowSet} object. Samples are arranged in order of increasing OVA
+#'   concentration.
 #'
-#'   Pooled DCs & T cells were labelled as follows: \itemize{
-#'   \item{\strong{Hoechst 33258} a DNA intercalating dye which fluoresces in
-#'   Alexa Fluor 405-A and Alexa Fluor 430-A channels.}
-#'   \item{\strong{CD11c-APC-Cy7} to identify DCs within the samples.}
-#'   \item{\strong{Va2-PE} to identify cells which express the OVA-specific TCR
-#'   (i.e. OT-I and OT-II T cells).} \item{\strong{CD8-FITC} to identify the
-#'   CD8+ OT-I transgenic T cells (Alexa Fluor 488-A).} \item{\strong{CD4-Alexa
-#'   Fluor 700} to identify the CD4+ OT-II transgenic T cells (Alexa Fluor
-#'   700-A).} \item{\strong{CD44-APC} to assess the extent of T cell activation
-#'   (Alexa Fluor 647-A).} \item{\strong{CD69-PerCP Cy5.5} to assess the extent
-#'   of T cell activation (7-AAD-A).} }}
+#'   Pooled DCs & T cells were labelled as follows: \itemize{ \item{\strong{Hoechst
+#'   33258} a DNA intercalating dye which fluoresces in Alexa Fluor 405-A and
+#'   Alexa Fluor 430-A channels.} \item{\strong{CD11c-APC-Cy7} to identify DCs within the
+#'   samples.} \item{\strong{Va2-PE} to identify cells which express the OVA-specific TCR
+#'   (i.e. OT-I and OT-II T cells).} \item{\strong{CD8-FITC} to identify the CD8+ OT-I
+#'   transgenic T cells (Alexa Fluor 488-A).} \item{\strong{CD4-Alexa Fluor 700} to
+#'   identify the CD4+ OT-II transgenic T cells (Alexa Fluor 700-A).}
+#'   \item{\strong{CD44-APC} to assess the extent of T cell activation (Alexa Fluor 647-A).}
+#'   \item{\strong{CD69-PerCP Cy5.5} to assess the extent of T cell activation (7-AAD-A).} }}
 #' @source The data comes from an in vitro OT-I/II T cell activation assay.
 #' @seealso \link{CytoRSuiteData}
 #' @seealso \link{Compensation}
@@ -102,18 +95,15 @@ NULL
 
 
 
-#' @title CytoRSuite gatingTemplate used gate the Compensation data set.
-#' @description A gatingTemplate used to gate the Compensation data set prior to
+#' @title CytoRSuite gatingTemplate used gate the Compensation dataset.
+#' @description A gatingTemplate used to gate the Compensation dataset prior to
 #'   spillover calculation.
 #' @name Compensation_gatingTemplate
 #' @docType data
 #' @title Descriptive data title
-#' @format a \code{gatingTemplate} containing the following 3 gates: \itemize{
-#'   \item{\strong{nonDebris}} \item{\strong{Cells}} \ item{\strong{Single
-#'   Cells}}}
-#' @source The compensation gatingTemplate was generated using CytoRSuite to
-#'   gate the compensation controls prior to claculation of fluorescent
-#'   spillover.
+#' @format a \code{gatingTemplate} containing the following 2 gates: \itemize{
+#'   \item{\strong{Cells}} \item{\strong{Single Cells}}}
+#' @source Some compensation controls from an experiment.
 #' @seealso \link{CytoRSuiteData}
 #' @seealso \link{Compensation}
 #' @author Dillon Hammill (Dillon.Hammill@anu.edu.au)
